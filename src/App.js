87e5d89b474f59df7ed655a55ca5417e09912b4f
Devanee Reid <devaneereid@gmail.com>
2020-01-17 15:57:16 -0700

    finished mvp working on removeitem now

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 import {ProductContext} from './contexts/ProductContext';
+import {CartContext} from './contexts/CartContext';
 
 function App() {
 	const [products] = useState(data);
@@ -17,16 +18,21 @@ function App() {
 		setCart([...cart, item]);
 	};
 
+	const removeItem = itemId => {
+		setCart(cart.filter(item => item.id !== itemId))
+	}
+
 	return (
 		<ProductContext.Provider value={{products, addItem}}>
-			<div className="App">
-				<Navigation cart={cart} />
+			<CartContext.Provider value={{cart}}>
+				<div className="App">
+					<Navigation cart={cart} />
 
-				{/* Routes */}
-				<Route exact path="/" component={Products} />
-					<Route path="/cart" render={() => 
-				<ShoppingCart cart={cart} />} />
-			</div>
+					{/* Routes */}
+					<Route exact path="/" component={Products} />
+						<Route path="/cart" component={ShoppingCart} />
+				</div>
+			</CartContext.Provider>
 		</ProductContext.Provider>
 	);
 }
